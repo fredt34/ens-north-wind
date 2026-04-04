3c64a47a0baef39bfb7cf660f8f95e87b6eb1b9b
FredT <fred.thu@gmail.com>
2026-04-04 08:46:33 +0200

    Update get-prediction.py

--- get-prediction.py
+++ get-prediction.py
@@ -46,6 +46,7 @@ hourly_data["wind_direction_80m"] = hourly_wind_direction_80m
 hourly_data["temperature_80m"] = hourly_temperature_80m
 
 hourly_dataframe = pd.DataFrame(data = hourly_data)
-pd.set_option("display.max_rows", None)
-pd.set_option("display.max_columns", None)
-print("\nHourly data\n", hourly_dataframe)
+# pd.set_option("display.max_rows", None)
+#pd.set_option("display.max_columns", None)
+#print("\nHourly data\n", hourly_dataframe)
+print(hourly_dataframe.to_string())
